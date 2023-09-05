aa73a4f7d0b77f67a7eb2dddd1281b0e3b3aba2c
Terry Ott <Terry.Ott@letsvalidate.org>
2023-09-05 17:05:19 +0000

    Checkin before we change the build OS and wreck the world

--- src/app/letsv-certchecker.go
+++ src/app/letsv-certchecker.go
@@ -7,7 +7,7 @@ import (
 
     //"github.com/aws/aws-sdk-go-v2/aws"
     "github.com/aws/aws-sdk-go-v2/config"
-    //"github.com/aws/aws-sdk-go-v2/service/ssm"
+    "github.com/aws/aws-sdk-go-v2/service/ssm"
 )
 
 
@@ -16,21 +16,17 @@ import (
 func main() {
     // Oh shit is it this? Bad C lib? https://github.com/aws/aws-sdk-go/issues/4835#issuecomment-1544383910
 
-    fmt.Println("Starting up")
-    myContext := context.TODO()
-    //_ = context.TODO()
-    //cfg, err := config.LoadDefaultConfig(myContext, config.WithRegion("us-east-2") )
-    _, err := config.LoadDefaultConfig(myContext, config.WithRegion("us-east-2") )
+    //fmt.Println("Starting up")
+    cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-2") )
 
     if err != nil {
         log.Fatalf("Unable to load SDK config: %v", err)
     }
 
-    /*
     // Get the SSM client
     //ssmClient := ssm.NewFromConfig(cfg)
-    //ssmClient := ssm.NewFromConfig(cfg)
     _ = ssm.NewFromConfig(cfg)
+    /*
 
     // Docs for this stuff: https://pkg.go.dev/github.com/aws/aws-sdk-go-v2/service/ssm#Client.GetParameters
 
