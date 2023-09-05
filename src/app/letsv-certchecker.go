aa63059b00ec71b70b2074f012977f134cf96f17
Terry Ott <Terry.Ott@letsvalidate.org>
2023-09-05 19:46:10 +0000

    Wow we actually pulled our param without having to do any permissions/credentials gynmastics. I'm kind of amazed

--- src/app/letsv-certchecker.go
+++ src/app/letsv-certchecker.go
@@ -4,7 +4,6 @@ import (
     "context"
     "fmt"
     "log"
-    "strconv"
 
     //"github.com/aws/aws-sdk-go-v2/aws"
     "github.com/aws/aws-sdk-go-v2/config"
@@ -15,9 +14,6 @@ import (
 
 
 func main() {
-    // Oh shit is it this? Bad C lib? https://github.com/aws/aws-sdk-go/issues/4835#issuecomment-1544383910
-
-    //fmt.Println("Starting up")
     cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-2") )
 
     if err != nil {
@@ -26,12 +22,16 @@ func main() {
 
     // Get the SSM client
     ssmClient := ssm.NewFromConfig(cfg)
-    //_ = ssm.NewFromConfig(cfg)
 
     // Docs for this stuff: https://pkg.go.dev/github.com/aws/aws-sdk-go-v2/service/ssm#Client.GetParameters
 
     // Get PGSQL params from the client
-    parameterNames := []string{"/letsvalidate/db/aws/us-east-2/dbhost"}
+    parameterNames := []string{
+        "/letsvalidate/db/aws/us-east-2/dbhost",
+        "/letsvalidate/db/aws/us-east-2/dbname",
+        "/letsvalidate/db/aws/us-east-2/dbpassword",
+        "/letsvalidate/db/aws/us-east-2/dbuser",
+    }
 
     // Intentionally null pointer -- GetParameters ignores this param for
     // string types, but it has to get passed to compile
@@ -48,17 +48,11 @@ func main() {
         log.Fatalf("Could not get params")
     }
 
-    fmt.Println("Whoah got a param?!?!?")
-
-    // Print invalid parameters
-    fmt.Println("Length of invalid parameters array : " + strconv.Itoa(len(paramOutput.InvalidParameters)) )
-    fmt.Println("Length of parameter details array  : " + strconv.Itoa(len(paramOutput.Parameters)) )
-
-    // Did we really get it
-    parameterName   := *paramOutput.Parameters[0].Name;
-    parameterValue  := *paramOutput.Parameters[0].Value
-
-    fmt.Println("Got parameter with name \"" + parameterName + "\" and value \"" + parameterValue + "\"" )
+    dbParams := make(map[string]string)
+    dbParamsKeys := []string{ "dbhost", "dbname", "dbpassword", "dbuser" }
+    for idx, currParam := range paramOutput.Parameters {
+        dbParams[dbParamKeys[idx]] = *currParam.Value
+    }
 
 
     // Connect to SSM to get PGSQL params
